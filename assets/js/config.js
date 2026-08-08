78c39e619c87a8ca3d42b6081855c8c01df8148e
postingan-z <postingan.bolapelangi@gmail.com>
2026-08-09 05:24:09 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,7 +4,7 @@
  * Contoh: https://script.google.com/macros/s/AKfycbxxxxxxxxxxxxxxxxxxxxxx/exec
  */
 const CONFIG = {
-  API_URL: 'GANTI_DENGAN_URL_WEB_APP_ANDA',
+  API_URL: 'https://script.google.com/macros/s/AKfycbyERB6e2dgINlzPDbdo3Fp7x3D5IFwU29doPLgiDc_hqtKaWxbaolYeIaT9GPGJw0S3/exec',
   POLLING_INTERVAL_MS: 4000, // 3-5 detik
   WHATSAPP_ADMIN_NUMBER: '62812xxxxxxx' // nomor WA admin (format internasional, tanpa +)
 };
